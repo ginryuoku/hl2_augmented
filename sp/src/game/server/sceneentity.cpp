f61151839e6e97631eead546245feab2c8bd89be
Michelle Darcy <silverpower.1@gmail.com>
2014-01-14 03:18:26 -0500

    And it turns out that trying to patch in random code from SDK2007 without thinking is a bad idea. Who knew?

--- sp/src/game/server/sceneentity.cpp
+++ sp/src/game/server/sceneentity.cpp
@@ -3345,44 +3345,36 @@ bool CSceneEntity::ShouldNetwork() const
 
 CChoreoScene *CSceneEntity::LoadScene( const char *filename, IChoreoEventCallback *pCallback )
 {
+	DevMsg( 2, "Blocking load of scene from '%s'\n", filename );
+
 	char loadfile[MAX_PATH];
 	Q_strncpy( loadfile, filename, sizeof( loadfile ) );
 	Q_SetExtension( loadfile, ".vcd", sizeof( loadfile ) );
 	Q_FixSlashes( loadfile );
- 
-	void *pBuffer = 0;
-	CChoreoScene *pScene;
- 
-	int fileSize = filesystem->ReadFileEx( loadfile, "MOD", &pBuffer, true );
-	if (fileSize)
+
+	// binary compiled vcd
+	void *pBuffer;
+	int fileSize;
+	if ( !CopySceneFileIntoMemory( loadfile, &pBuffer, &fileSize ) )
 	{
-		g_TokenProcessor.SetBuffer((char*)pBuffer);
-		pScene = ChoreoLoadScene( loadfile, NULL, &g_TokenProcessor, LocalScene_Printf );
+		MissingSceneWarning( loadfile );
+		return NULL;
+	}
+
+	CChoreoScene *pScene = new CChoreoScene( NULL );
+	CUtlBuffer buf( pBuffer, fileSize, CUtlBuffer::READ_ONLY );
+	if ( !pScene->RestoreFromBinaryBuffer( buf, loadfile, &g_ChoreoStringPool ) )
+	{
+		Warning( "CSceneEntity::LoadScene: Unable to load binary scene '%s'\n", loadfile );
+		delete pScene;
+		pScene = NULL;
 	}
 	else
-	{
-		// binary compiled vcd
-		pScene = new CChoreoScene( NULL );
-		if ( !CopySceneFileIntoMemory( loadfile, &pBuffer, &fileSize ) )
-		{
-			MissingSceneWarning( loadfile );
-			return NULL;
-		}
-		CUtlBuffer buf( pBuffer, fileSize, CUtlBuffer::READ_ONLY );
-		if ( !pScene->RestoreFromBinaryBuffer( buf, loadfile, &g_ChoreoStringPool ) )
-		{
-			Warning( "CSceneEntity::LoadScene: Unable to load scene '%s'\n", loadfile );
-			delete pScene;
-			pScene = NULL;
-		}
-	}
- 
-	if(pScene)
 	{
 		pScene->SetPrintFunc( LocalScene_Printf );
 		pScene->SetEventCallbackInterface( pCallback );
 	}
- 
+
 	FreeSceneFileMemory( pBuffer );
 	return pScene;
 }
