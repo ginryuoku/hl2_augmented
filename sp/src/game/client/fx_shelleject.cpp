3f1e67f8e25777d016898a7ac4494a3f7c7d3a7a
Michelle Darcy <michelle.darcy@pcc.edu>
2014-01-27 14:40:49 -0500

    Weapons: Add in the old CS:S shelleject patch I did for Sofia (and myself). :)

--- sp/src/game/client/fx_shelleject.cpp
+++ sp/src/game/client/fx_shelleject.cpp
@@ -57,3 +57,50 @@ void ShotgunShellEjectCallback( const CEffectData &data )
 DECLARE_CLIENT_EFFECT( "ShotgunShellEject", ShotgunShellEjectCallback );
 
 
+void CStrike_EjectBrass( int shell, const CEffectData &data )
+{
+	C_BasePlayer *pPlayer = C_BasePlayer::GetLocalPlayer();
+
+	if( !pPlayer )
+		return;
+
+	tempents->CSEjectBrass( data.m_vOrigin, data.m_vAngles, data.m_fFlags, shell, pPlayer );
+}
+
+void FX_EjectBrass_9mm_Callback( const CEffectData &data )
+{
+	CStrike_EjectBrass( CS_SHELL_9MM, data );
+}
+
+void FX_EjectBrass_12Gauge_Callback( const CEffectData &data )
+{
+	CStrike_EjectBrass( CS_SHELL_12GAUGE, data );
+}
+
+void FX_EjectBrass_57_Callback( const CEffectData &data )
+{
+	CStrike_EjectBrass( CS_SHELL_57, data );
+}
+
+void FX_EjectBrass_556_Callback( const CEffectData &data )
+{
+	CStrike_EjectBrass( CS_SHELL_556, data );
+}
+
+void FX_EjectBrass_762Nato_Callback( const CEffectData &data )
+{
+	CStrike_EjectBrass( CS_SHELL_762NATO, data );
+}
+
+void FX_EjectBrass_338Mag_Callback( const CEffectData &data )
+{
+	CStrike_EjectBrass( CS_SHELL_338MAG, data );
+}
+
+
+DECLARE_CLIENT_EFFECT( "EjectBrass_9mm",		FX_EjectBrass_9mm_Callback );
+DECLARE_CLIENT_EFFECT( "EjectBrass_12Gauge",	FX_EjectBrass_12Gauge_Callback );
+DECLARE_CLIENT_EFFECT( "EjectBrass_57",			FX_EjectBrass_57_Callback );
+DECLARE_CLIENT_EFFECT( "EjectBrass_556",		FX_EjectBrass_556_Callback );
+DECLARE_CLIENT_EFFECT( "EjectBrass_762Nato",	FX_EjectBrass_762Nato_Callback );
+DECLARE_CLIENT_EFFECT( "EjectBrass_338Mag",		FX_EjectBrass_338Mag_Callback );
