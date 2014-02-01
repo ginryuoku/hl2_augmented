ddf6872587e5608eab71eab90d50db2c98b45f9d
Michelle Darcy <silverpower.1@gmail.com>
2014-02-01 07:26:00 -0500

    Inventory UI: Fiddled some with the layout.

--- sp/game/mod_hl2/html/invpanel/invpanel.js
+++ sp/game/mod_hl2/html/invpanel/invpanel.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
 
 	$(function(){ //DOM Ready
-	 
+	
 		$(".gridster ul").gridster({
 		widget_margins: [5, 5],
-		widget_base_dimensions: [50, 50],
-		max_cols: 15,
-		min_cols: 15,
-		min_rows: 7
+		widget_base_dimensions: [60, 60],
+		min_cols: 16,
+		max_cols: 16,
+		min_rows: 8
 		});
 	
 	});
