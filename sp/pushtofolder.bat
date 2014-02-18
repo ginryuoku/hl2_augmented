81f85cc74bded80ccdb7737c37bbf51b86f87fbe
Michelle Darcy <silverpower.1@gmail.com>
2014-02-18 05:17:38 -0500

    Switching demo content to EP2.

--- sp/pushtofolder.bat
+++ sp/pushtofolder.bat
@@ -1,5 +1,6 @@
 @echo off
 
-echo d | xcopy /s/e/y ".\game\mod_hl2" "C:\Program Files (x86)\Steam\steamapps\sourcemods\comrade_erika"
+echo d | xcopy /s/e/y ".\game\mod_episodic" "C:\Program Files (x86)\Steam\steamapps\sourcemods\comrade_erika"
+echo d | xcopy /s/e/y "..\..\ce_replacecontent\mod_episodic" "C:\Program Files (x86)\Steam\steamapps\sourcemods\comrade_erika"
 
 pause
