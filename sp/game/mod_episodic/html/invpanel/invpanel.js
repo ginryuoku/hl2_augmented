7868cbb9cf1e519cd180295e61a751e363071546
Michelle Darcy <silverpower.1@gmail.com>
2014-02-22 06:44:34 -0500

    Adjustments for Xcode building.

--- sp/game/mod_episodic/html/invpanel/invpanel.js
+++ sp/game/mod_episodic/html/invpanel/invpanel.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 	
 		$(".gridster ul").gridster({
 		widget_margins: [5, 5],
-		widget_base_dimensions: [60, 60],
+		widget_base_dimensions: [64, 64],
 		min_cols: 16,
 		max_cols: 16,
 		min_rows: 8
