84e2774975a49e378f91008b40e383ba9c6a2138
Michelle Darcy <silverpower.1@gmail.com>
2014-03-02 15:08:05 -0500

    Initial implementation of client-side inventory code.

---
+++ sp/src/game/client/comrade_erika/c_baseinventory.cpp
@@ -0,0 +1,64 @@
+//
+//  c_baseinventory.cpp
+//  games
+//
+//  Created by Michelle Darcy on 3/2/14.
+//
+//
+
+#include "c_baseinventory.h"
+
+CBaseInventory::CBaseInventory()
+{
+	PurgeAllObjects();
+}
+
+void CBaseInventory::PurgeObject( int element )
+{
+	ItemID[element] = -1;
+	ItemCap[element] = 0;
+	ItemMaxCap[element] = 0;
+}
+
+void CBaseInventory::PurgeAllObjects()
+{
+	for ( int i = 0; i < 100; ++i)
+	{
+		PurgeObject(i);
+	}
+}
+
+int CBaseInventory::GetItemID( int element )
+{
+	return ItemID[element];
+}
+
+int CBaseInventory::GetItemCapacity( int element )
+{
+	return ItemCap[element];
+}
+
+int CBaseInventory::GetItemMaxCapacity( int element )
+{
+	return ItemMaxCap[element];
+}
+
+void CBaseInventory::NewObject( int ObjectIndex, int NewItemID, int NewItemCap, int NewItemMaxCap )
+{
+	ItemID[ObjectIndex] = NewItemID;
+	ItemCap[ObjectIndex] = NewItemCap;
+	ItemMaxCap[ObjectIndex] = NewItemMaxCap;
+	DevMsg("Client: Created new object at position %d of type %d with capacity %d and max capacity %d\n", ObjectIndex, NewItemID, NewItemCap, NewItemMaxCap);
+}
+
+int CBaseInventory::FindFirstFreeObject()
+{
+	for (int i = 0; i < MAX_INVENTORY; ++i)
+	{
+		if (ItemID[i] == -1)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
---
+++ sp/src/game/client/comrade_erika/c_baseinventory.h
@@ -0,0 +1,40 @@
+//
+//  c_baseinventory.h
+//  games
+//
+//  Created by Michelle Darcy on 3/2/14.
+//
+//
+
+#ifndef __games__c_baseinventory__
+#define __games__c_baseinventory__
+
+#include "cbase.h"
+
+#define MAX_INVENTORY 100
+
+class CBaseInventory
+{
+public:
+	CBaseInventory();
+	
+	DECLARE_CLASS_NOBASE( CBaseInventory );
+	//DECLARE_SERVERCLASS();
+	
+	void PurgeObject( int element );
+	void PurgeAllObjects();
+	
+	int GetItemID( int element );
+	int GetItemCapacity( int element );
+	int GetItemMaxCapacity( int element );
+ 	
+	int FindFirstFreeObject();
+	
+	void NewObject( int ObjectIndex, int NewItemID, int NewItemCap, int NewItemMaxCap );
+private:
+	int ItemID[MAX_INVENTORY];
+	int ItemCap[MAX_INVENTORY];
+	int ItemMaxCap[MAX_INVENTORY];
+};
+
+#endif /* defined(__games__c_baseinventory__) */
