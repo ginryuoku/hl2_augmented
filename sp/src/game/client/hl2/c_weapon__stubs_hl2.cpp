6a9f29eca135af6b4d6458e8c80319ba478dbf88
Michelle Darcy <michelle.darcy@pcc.edu>
2014-12-26 23:39:11 -0500

    Adds FN Five-Seven to game.

--- sp/src/game/client/hl2/c_weapon__stubs_hl2.cpp
+++ sp/src/game/client/hl2/c_weapon__stubs_hl2.cpp
@@ -45,6 +45,7 @@ STUB_WEAPON_CLASS( weapon_oldmanharpoon, WeaponOldManHarpoon, C_WeaponCitizenPac
 #endif
 
 STUB_WEAPON_CLASS( weapon_hgun_m9, WeaponM9, C_BaseHLCombatWeapon );
+STUB_WEAPON_CLASS(weapon_hgun_fiveseven, WeaponFiveSeven, C_BaseHLCombatWeapon);
 
 STUB_WEAPON_CLASS( weapon_mag_mr96, WeaponMR96, C_BaseHLCombatWeapon );
 
