901b6b1b0e9b1104985e7c4c569b3ec2e8a62f20
Michelle Darcy <michelle.darcy@pcc.edu>
2015-01-11 20:19:01 -0500

    New weapon: Vltor SBR - a Vltor LE/MIL AR-15 platform built with an 11.5" upper, A2 muzzle device and suppressor.

--- sp/src/game/client/hl2/c_weapon__stubs_hl2.cpp
+++ sp/src/game/client/hl2/c_weapon__stubs_hl2.cpp
@@ -62,6 +62,7 @@ STUB_WEAPON_CLASS(weapon_pdw_mp7a1, WeaponMP7A1, C_HLMachineGun);
 STUB_WEAPON_CLASS( weapon_pdw_p90, WeaponP90, C_HLMachineGun );
 
 STUB_WEAPON_CLASS( weapon_arifle_m4a1, WeaponM4A1, C_HLMachineGun );
+STUB_WEAPON_CLASS(weapon_arifle_vltorsbr, WeaponVltorSBR, C_HLMachineGun);
 
 STUB_WEAPON_CLASS( weapon_launcher_m79, WeaponLauncherM79, C_BaseHLCombatWeapon );
 STUB_WEAPON_CLASS( weapon_dissolver, WeaponDissolver, C_BaseHLCombatWeapon );
---
+++ sp/src/game/server/comrade_erika/weapon_arifle_vltorsbr.cpp
@@ -0,0 +1,408 @@
+//========= Copyright © 1996-2005, Valve Corporation, All rights reserved. ============//
+//
+// Purpose: 
+//
+//=============================================================================//
+
+#include "cbase.h"
+#include "basehlcombatweapon.h"
+#include "NPCevent.h"
+#include "basecombatcharacter.h"
+#include "AI_BaseNPC.h"
+#include "player.h"
+#include "game.h"
+#include "in_buttons.h"
+#include "AI_Memory.h"
+#include "soundent.h"
+#include "rumble_shared.h"
+#include "gamestats.h"
+
+// memdbgon must be the last include file in a .cpp file!!!
+#include "tier0/memdbgon.h"
+
+extern ConVar    sk_plr_dmg_smg1_grenade;
+
+class CWeaponVltorSBR : public CHLMachineGun
+{
+	DECLARE_DATADESC();
+public:
+	DECLARE_CLASS(CWeaponVltorSBR, CHLMachineGun);
+
+	CWeaponVltorSBR();
+
+	DECLARE_SERVERCLASS();
+
+	void	Precache(void);
+	void	AddViewKick(void);
+
+	int		GetMinBurst() { return 2; }
+	int		GetMaxBurst() { return 5; }
+
+	virtual void Equip(CBaseCombatCharacter *pOwner);
+	bool	Reload(void);
+
+	float	GetFireRate(void) { return 0.075f; }	// 13.333hz, 800rpm
+	int		CapabilitiesGet(void) { return bits_CAP_WEAPON_RANGE_ATTACK1; }
+
+	Activity	GetPrimaryAttackActivity(void);
+
+	virtual const Vector& GetBulletSpread(void)
+	{
+		static const Vector cone = VECTOR_CONE_1DEGREES / 60;
+		return cone;
+	}
+
+	const WeaponProficiencyInfo_t *GetProficiencyValues();
+
+	void FireNPCPrimaryAttack(CBaseCombatCharacter *pOperator, Vector &vecShootOrigin, Vector &vecShootDir);
+	void Operator_ForceNPCFire(CBaseCombatCharacter  *pOperator, bool bSecondary);
+	void Operator_HandleAnimEvent(animevent_t *pEvent, CBaseCombatCharacter *pOperator);
+	void PrimaryAttack(void);
+
+	DECLARE_ACTTABLE();
+
+};
+
+IMPLEMENT_SERVERCLASS_ST(CWeaponVltorSBR, DT_WeaponVltorSBR)
+END_SEND_TABLE()
+
+LINK_ENTITY_TO_CLASS(weapon_arifle_vltorsbr, CWeaponVltorSBR);
+PRECACHE_WEAPON_REGISTER(weapon_arifle_vltorsbr);
+
+BEGIN_DATADESC(CWeaponVltorSBR)
+END_DATADESC()
+
+acttable_t	CWeaponVltorSBR::m_acttable[] =
+{
+	{ ACT_RANGE_ATTACK1, ACT_RANGE_ATTACK_SMG1, true },
+	{ ACT_RELOAD, ACT_RELOAD_SMG1, true },
+	{ ACT_IDLE, ACT_IDLE_SMG1, true },
+	{ ACT_IDLE_ANGRY, ACT_IDLE_ANGRY_SMG1, true },
+
+	{ ACT_WALK, ACT_WALK_RIFLE, true },
+	{ ACT_WALK_AIM, ACT_WALK_AIM_RIFLE, true },
+
+	// Readiness activities (not aiming)
+	{ ACT_IDLE_RELAXED, ACT_IDLE_SMG1_RELAXED, false },//never aims
+	{ ACT_IDLE_STIMULATED, ACT_IDLE_SMG1_STIMULATED, false },
+	{ ACT_IDLE_AGITATED, ACT_IDLE_ANGRY_SMG1, false },//always aims
+
+	{ ACT_WALK_RELAXED, ACT_WALK_RIFLE_RELAXED, false },//never aims
+	{ ACT_WALK_STIMULATED, ACT_WALK_RIFLE_STIMULATED, false },
+	{ ACT_WALK_AGITATED, ACT_WALK_AIM_RIFLE, false },//always aims
+
+	{ ACT_RUN_RELAXED, ACT_RUN_RIFLE_RELAXED, false },//never aims
+	{ ACT_RUN_STIMULATED, ACT_RUN_RIFLE_STIMULATED, false },
+	{ ACT_RUN_AGITATED, ACT_RUN_AIM_RIFLE, false },//always aims
+
+	// Readiness activities (aiming)
+	{ ACT_IDLE_AIM_RELAXED, ACT_IDLE_SMG1_RELAXED, false },//never aims	
+	{ ACT_IDLE_AIM_STIMULATED, ACT_IDLE_AIM_RIFLE_STIMULATED, false },
+	{ ACT_IDLE_AIM_AGITATED, ACT_IDLE_ANGRY_SMG1, false },//always aims
+
+	{ ACT_WALK_AIM_RELAXED, ACT_WALK_RIFLE_RELAXED, false },//never aims
+	{ ACT_WALK_AIM_STIMULATED, ACT_WALK_AIM_RIFLE_STIMULATED, false },
+	{ ACT_WALK_AIM_AGITATED, ACT_WALK_AIM_RIFLE, false },//always aims
+
+	{ ACT_RUN_AIM_RELAXED, ACT_RUN_RIFLE_RELAXED, false },//never aims
+	{ ACT_RUN_AIM_STIMULATED, ACT_RUN_AIM_RIFLE_STIMULATED, false },
+	{ ACT_RUN_AIM_AGITATED, ACT_RUN_AIM_RIFLE, false },//always aims
+	//End readiness activities
+
+	{ ACT_WALK_AIM, ACT_WALK_AIM_RIFLE, true },
+	{ ACT_WALK_CROUCH, ACT_WALK_CROUCH_RIFLE, true },
+	{ ACT_WALK_CROUCH_AIM, ACT_WALK_CROUCH_AIM_RIFLE, true },
+	{ ACT_RUN, ACT_RUN_RIFLE, true },
+	{ ACT_RUN_AIM, ACT_RUN_AIM_RIFLE, true },
+	{ ACT_RUN_CROUCH, ACT_RUN_CROUCH_RIFLE, true },
+	{ ACT_RUN_CROUCH_AIM, ACT_RUN_CROUCH_AIM_RIFLE, true },
+	{ ACT_GESTURE_RANGE_ATTACK1, ACT_GESTURE_RANGE_ATTACK_SMG1, true },
+	{ ACT_RANGE_ATTACK1_LOW, ACT_RANGE_ATTACK_SMG1_LOW, true },
+	{ ACT_COVER_LOW, ACT_COVER_SMG1_LOW, false },
+	{ ACT_RANGE_AIM_LOW, ACT_RANGE_AIM_SMG1_LOW, false },
+	{ ACT_RELOAD_LOW, ACT_RELOAD_SMG1_LOW, false },
+	{ ACT_GESTURE_RELOAD, ACT_GESTURE_RELOAD_SMG1, true },
+};
+
+IMPLEMENT_ACTTABLE(CWeaponVltorSBR);
+
+//=========================================================
+CWeaponVltorSBR::CWeaponVltorSBR()
+{
+	m_fMinRange1 = 0;// No minimum range. 
+	m_fMaxRange1 = 1400;
+
+	m_bAltFiresUnderwater = false;
+
+	m_nFireMode = 0;
+	m_nShotsLeft = m_nFireMode;
+	m_nBurstRate = 3;
+	m_nBurstToothState = m_nBurstRate;
+	m_bFMAutomatic = true;
+	m_bHasSemiAuto = true;
+	m_bHasBurstGroup = false;
+	m_bHKBurstType = false; // ARs lack a sane burst group.
+	m_bIsClosedBolt = true;
+	m_bManuallyOperated = false;
+	m_bFMCycleDirection = true;
+	m_bUsesSwitchToChange = true;
+	m_bAcceleratesFA = false;
+	m_fAlternateBurstROF = 10.0f;
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CWeaponVltorSBR::Precache(void)
+{
+	BaseClass::Precache();
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: Give this weapon longer range when wielded by an ally NPC.
+//-----------------------------------------------------------------------------
+void CWeaponVltorSBR::Equip(CBaseCombatCharacter *pOwner)
+{
+	if (pOwner->Classify() == CLASS_PLAYER_ALLY)
+	{
+		m_fMaxRange1 = 3000;
+	}
+	else
+	{
+		m_fMaxRange1 = 1400;
+	}
+
+	BaseClass::Equip(pOwner);
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CWeaponVltorSBR::FireNPCPrimaryAttack(CBaseCombatCharacter *pOperator, Vector &vecShootOrigin, Vector &vecShootDir)
+{
+	// FIXME: use the returned number of bullets to account for >10hz firerate
+	WeaponSoundRealtime(SINGLE_NPC);
+
+	CSoundEnt::InsertSound(SOUND_COMBAT | SOUND_CONTEXT_GUNFIRE, pOperator->GetAbsOrigin(), SOUNDENT_VOLUME_MACHINEGUN, 0.2, pOperator, SOUNDENT_CHANNEL_WEAPON, pOperator->GetEnemy());
+	pOperator->FireBullets(1, vecShootOrigin, vecShootDir, VECTOR_CONE_PRECALCULATED,
+		MAX_TRACE_LENGTH, m_iPrimaryAmmoType, 2, entindex(), 0);
+
+	pOperator->DoMuzzleFlash();
+	m_iClip1 = m_iClip1 - 1;
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CWeaponVltorSBR::Operator_ForceNPCFire(CBaseCombatCharacter *pOperator, bool bSecondary)
+{
+	// Ensure we have enough rounds in the clip
+	m_iClip1++;
+
+	Vector vecShootOrigin, vecShootDir;
+	QAngle	angShootDir;
+	GetAttachment(LookupAttachment("muzzle"), vecShootOrigin, angShootDir);
+	AngleVectors(angShootDir, &vecShootDir);
+	FireNPCPrimaryAttack(pOperator, vecShootOrigin, vecShootDir);
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CWeaponVltorSBR::Operator_HandleAnimEvent(animevent_t *pEvent, CBaseCombatCharacter *pOperator)
+{
+	switch (pEvent->event)
+	{
+	case EVENT_WEAPON_SMG1:
+	{
+		Vector vecShootOrigin, vecShootDir;
+		QAngle angDiscard;
+
+		// Support old style attachment point firing
+		if ((pEvent->options == NULL) || (pEvent->options[0] == '\0') || (!pOperator->GetAttachment(pEvent->options, vecShootOrigin, angDiscard)))
+		{
+			vecShootOrigin = pOperator->Weapon_ShootPosition();
+		}
+
+		CAI_BaseNPC *npc = pOperator->MyNPCPointer();
+		ASSERT(npc != NULL);
+		vecShootDir = npc->GetActualShootTrajectory(vecShootOrigin);
+
+		FireNPCPrimaryAttack(pOperator, vecShootOrigin, vecShootDir);
+	}
+	break;
+
+	/*//FIXME: Re-enable
+	case EVENT_WEAPON_AR2_GRENADE:
+	{
+	CAI_BaseNPC *npc = pOperator->MyNPCPointer();
+
+	Vector vecShootOrigin, vecShootDir;
+	vecShootOrigin = pOperator->Weapon_ShootPosition();
+	vecShootDir = npc->GetShootEnemyDir( vecShootOrigin );
+
+	Vector vecThrow = m_vecTossVelocity;
+
+	CGrenadeAR2 *pGrenade = (CGrenadeAR2*)Create( "grenade_ar2", vecShootOrigin, vec3_angle, npc );
+	pGrenade->SetAbsVelocity( vecThrow );
+	pGrenade->SetLocalAngularVelocity( QAngle( 0, 400, 0 ) );
+	pGrenade->SetMoveType( MOVETYPE_FLYGRAVITY );
+	pGrenade->m_hOwner			= npc;
+	pGrenade->m_pMyWeaponAR2	= this;
+	pGrenade->SetDamage(sk_npc_dmg_ar2_grenade.GetFloat());
+
+	// FIXME: arrgg ,this is hard coded into the weapon???
+	m_flNextGrenadeCheck = gpGlobals->curtime + 6;// wait six seconds before even looking again to see if a grenade can be thrown.
+
+	m_iClip2--;
+	}
+	break;
+	*/
+
+	default:
+		BaseClass::Operator_HandleAnimEvent(pEvent, pOperator);
+		break;
+	}
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+// Output : Activity
+//-----------------------------------------------------------------------------
+Activity CWeaponVltorSBR::GetPrimaryAttackActivity(void)
+{
+	return ACT_VM_PRIMARYATTACK;
+}
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+bool CWeaponVltorSBR::Reload(void)
+{
+	return DefaultReload(GetMaxClip1(), GetMaxClip2(), ACT_VM_RELOAD);
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CWeaponVltorSBR::AddViewKick(void)
+{
+#define	EASY_DAMPEN			0.5f
+#define	MAX_VERTICAL_KICK	1.0f	//Degrees
+#define	SLIDE_LIMIT			2.0f	//Seconds
+
+	//Get the view kick
+	CBasePlayer *pPlayer = ToBasePlayer(GetOwner());
+
+	if (pPlayer == NULL)
+		return;
+
+	DoMachineGunKick(pPlayer, EASY_DAMPEN, MAX_VERTICAL_KICK, m_fFireDuration, SLIDE_LIMIT);
+}
+
+
+
+//-----------------------------------------------------------------------------
+const WeaponProficiencyInfo_t *CWeaponVltorSBR::GetProficiencyValues()
+{
+	static WeaponProficiencyInfo_t proficiencyTable[] =
+	{
+		{ 7.0, 0.75 },
+		{ 5.00, 0.75 },
+		{ 10.0 / 3.0, 0.75 },
+		{ 5.0 / 3.0, 0.75 },
+		{ 1.00, 1.0 },
+	};
+
+	COMPILE_TIME_ASSERT(ARRAYSIZE(proficiencyTable) == WEAPON_PROFICIENCY_PERFECT + 1);
+
+	return proficiencyTable;
+}
+
+void CWeaponVltorSBR::PrimaryAttack(void)
+{
+	// If there is less than 1 shot left in this firing mode... return
+	if (m_nShotsLeft < 1)
+		return;
+
+	// If the firing mode is less than four, remove one from the shots left counter...
+	if (m_nFireMode < 3)
+		m_nShotsLeft--;
+
+	// and update the burst tooth state.
+	--m_nBurstToothState;
+
+	// Only the player fires this way so we can cast
+	CBasePlayer *pPlayer = ToBasePlayer(GetOwner());
+	if (!pPlayer)
+		return;
+
+
+	// Abort here to handle burst and auto fire modes
+	if ((UsesClipsForAmmo1() && m_iClip1 == 0) || (!UsesClipsForAmmo1() && !pPlayer->GetAmmoCount(m_iPrimaryAmmoType)))
+		return;
+
+	m_nShotsFired++;
+
+	pPlayer->DoMuzzleFlash();
+
+	// To make the firing frame-rate independent, we may have to fire more than one bullet here on low-framerate systems, 
+	// especially if the weapon we're firing has a really fast rate of fire.
+	int iBulletsToFire = 0;
+	float fireRate = 0.5f;
+	// If I accelerate ROF in burst, and I'm in the appropriate fire mode 
+	// (Nikonov weapons in burst/FA, G11 in burst), use the alternate rate of fire.
+	if (m_fAlternateBurstROF < 5 && ((m_bAcceleratesFA && m_nFireMode > 1) || (!m_bAcceleratesFA && m_nFireMode == 2)))
+		fireRate = m_fAlternateBurstROF;
+	else
+		fireRate = GetFireRate();
+
+	// MUST call sound before removing a round from the clip of a CHLMachineGun
+	while (m_flNextPrimaryAttack <= gpGlobals->curtime)
+	{
+		WeaponSound(SINGLE, m_flNextPrimaryAttack);
+		m_flNextPrimaryAttack = m_flNextPrimaryAttack + fireRate;
+		iBulletsToFire++;
+	}
+
+	// Make sure we don't fire more than the amount in the clip, if this weapon uses clips
+	if (UsesClipsForAmmo1())
+	{
+		if (iBulletsToFire > m_iClip1)
+			iBulletsToFire = m_iClip1;
+		m_iClip1 -= iBulletsToFire;
+	}
+
+	m_iPrimaryAttacks++;
+	gamestats->Event_WeaponFired(pPlayer, true, GetClassname());
+
+	// Fire the bullets
+	FireBulletsInfo_t info;
+	info.m_iShots = iBulletsToFire;
+	info.m_vecSrc = pPlayer->Weapon_ShootPosition();
+	info.m_vecDirShooting = pPlayer->GetAutoaimVector(AUTOAIM_SCALE_DEFAULT);
+	info.m_vecSpread = pPlayer->GetAttackSpread(this);
+	info.m_flDistance = MAX_TRACE_LENGTH;
+	info.m_iAmmoType = m_iPrimaryAmmoType;
+	info.m_iTracerFreq = 2;
+	FireBullets(info);
+
+	//Factor in the view kick
+	AddViewKick();
+
+	// We're a suppressed weapon, which means we need to sound like one to our enemies.
+	CSoundEnt::InsertSound(SOUND_WORLD, GetAbsOrigin(), SOUNDENT_VOLUME_EMPTY, 0.2, GetOwner());
+
+	if (!m_iClip1 && pPlayer->GetAmmoCount(m_iPrimaryAmmoType) <= 0 &&
+		((!UsesMagazines() && pPlayer->m_pInventory.CountAllObjectsOfID(GetPrimaryAmmoID()) <= 0)) &&
+		(UsesMagazines() && pPlayer->m_pInventory.CountAllObjectContentsOfID(GetPrimaryMagazineID())))
+	{
+		// HEV suit - indicate out of ammo condition
+		pPlayer->SetSuitUpdate("!HEV_AMO0", FALSE, 0);
+	}
+
+	SendWeaponAnim(GetPrimaryAttackActivity());
+	pPlayer->SetAnimation(PLAYER_ATTACK1);
+
+	// Register a muzzleflash for the AI
+	pPlayer->SetMuzzleFlashTime(gpGlobals->curtime + 0.5);
+}
