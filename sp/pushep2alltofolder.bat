bf2b29f93747ab68ebe8235af524dcbdd49c4aa0
Michelle Darcy <michelle.darcy@pcc.edu>
2015-01-15 17:22:11 -0500

    Split episodic into EP1 and EP2 branches. Also, newlines in the batch files.

---
+++ sp/pushep2alltofolder.bat
@@ -0,0 +1,8 @@
+@echo off
+
+echo d | xcopy /s/e/y ".\game\mod_episodic" "C:\Program Files (x86)\Steam\steamapps\sourcemods\comrade_erika_ep2"
+echo d | xcopy /s/e/y ".\game\mod_ep2" "C:\Program Files (x86)\Steam\steamapps\sourcemods\comrade_erika_ep2"
+echo d | xcopy /s/e/y "..\..\ce_replacecontent\mod_hl2" "C:\Program Files (x86)\Steam\steamapps\sourcemods\comrade_erika_ep2"
+echo d | xcopy /s/e/y "..\..\ce_replacecontent\mod_episodic" "C:\Program Files (x86)\Steam\steamapps\sourcemods\comrade_erika_ep2"
+
+pause
