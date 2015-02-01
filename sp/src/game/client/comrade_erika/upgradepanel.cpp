1a695c118a8a1210305a56a08f3ded7c25bd385c
Michelle Darcy <michelle.darcy@pcc.edu>
2015-02-01 01:01:07 -0500

    Adds stats label for each category of enhancement.

--- sp/src/game/client/comrade_erika/upgradepanel.cpp
+++ sp/src/game/client/comrade_erika/upgradepanel.cpp
@@ -4,6 +4,11 @@
 using namespace vgui;
 #include <vgui/IVGui.h>
 #include <vgui_controls/Frame.h>
+#include <vgui_controls/Label.h>
+
+#include "c_basehlplayer.h"
+
+#define MAX_AUGS 5 //
 
 //CMyPanel class: Tutorial example class
 class CUpgradePanel : public vgui::Frame
@@ -21,13 +26,15 @@ protected:
 
 private:
 	//Other used VGUI control Elements:
-
+	Label *pLabel[MAX_AUGS - 1];
 };
 
 // Constructor: Initializes the Panel
 CUpgradePanel::CUpgradePanel(vgui::VPANEL parent)
 	: BaseClass(NULL, "UpgradePanel")
 {
+	char buffer[32];
+
 	SetParent(parent);
 
 	SetKeyBoardInputEnabled(true);
@@ -41,15 +48,21 @@ CUpgradePanel::CUpgradePanel(vgui::VPANEL parent)
 	SetSizeable(false);
 	SetMoveable(false);
 	SetVisible(true);
-
-
+	
 	SetScheme(vgui::scheme()->LoadSchemeFromFile("resource/SourceScheme.res", "SourceScheme"));
+
+	for (int i = 0; i < MAX_AUGS; ++i)
+	{
+		Q_snprintf(buffer, sizeof(buffer), "auglabel%i", i + 1);
+		pLabel[i] = new Label(this, buffer, "BLANK");
+	}
 
 	LoadControlSettings("resource/UI/upgradepanel.res");
 
 	vgui::ivgui()->AddTickSignal(GetVPanel(), 100);
 
 	DevMsg("UpgradePanel has been constructed.\n");
+
 }
 
 //Class: CMyPanelInterface Class. Used for construction.
@@ -84,6 +97,30 @@ void CUpgradePanel::OnTick()
 {
 	BaseClass::OnTick();
 	SetVisible(cl_showupgradepanel.GetBool());
+
+	CBasePlayer *pPlayer = ToBasePlayer(UTIL_PlayerByIndex(1));
+	if (pPlayer)
+	{
+		char buffer[256];
+		C_BaseHLPlayer *pHL2Player = dynamic_cast<C_BaseHLPlayer*>(pPlayer);
+		if (pHL2Player)
+		{
+			Q_snprintf(buffer, sizeof(buffer), "Next upgrade costs %i\nCurrent Upgrade %i / 10", (pPlayer->m_Local.m_iHealthUpgrades + 1) * 1000, pPlayer->m_Local.m_iHealthUpgrades);
+			pLabel[0]->SetText(buffer);
+
+			Q_snprintf(buffer, sizeof(buffer), "Next upgrade costs %i\nCurrent Upgrade %i / 10", (pPlayer->m_Local.m_iHealthSegmentUpgrades + 1) * 1000, pPlayer->m_Local.m_iHealthSegmentUpgrades);
+			pLabel[1]->SetText(buffer);
+
+			Q_snprintf(buffer, sizeof(buffer), "Next upgrade costs %i\nCurrent Upgrade %i / 14", (pPlayer->m_Local.m_iArmorUpgrades + 1) * 1000, pPlayer->m_Local.m_iArmorUpgrades);
+			pLabel[2]->SetText(buffer);
+
+			Q_snprintf(buffer, sizeof(buffer), "Next upgrade costs %i\nCurrent Upgrade %i / 10", (pPlayer->m_Local.m_iArmorSegmentUpgrades + 1) * 1000, pPlayer->m_Local.m_iArmorSegmentUpgrades);
+			pLabel[3]->SetText(buffer);
+
+			Q_snprintf(buffer, sizeof(buffer), "Next upgrade costs %i\nCurrent Upgrade %i / 10", (pHL2Player->m_HL2Local.m_iAuxPowerUpgradeCells + 1) * 2000, pHL2Player->m_HL2Local.m_iAuxPowerUpgradeCells);
+			pLabel[4]->SetText(buffer);
+		}
+	}
 }
 
 void CUpgradePanel::OnCommand(const char* pcCommand)
